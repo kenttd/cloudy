20db0d52c401df22193feaac73cc00ce077ea48e
kent <kenttravisdonovan@gmail.com>
2024-12-28 16:07:59 +0700

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
   experimental: {
     instrumentationHook: true,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
