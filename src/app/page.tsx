f94dc52895bd0f9d9c020b8b51e5bf75b1287bd8
kent <kenttravisdonovan@gmail.com>
2024-12-28 15:51:10 +0700

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,3 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import { FileUploader } from "@/components/file-uploader";
-import Logo from "@/assets/logo.png";
-import Image from "next/image";
-import SideBar from "@/components/SideBar";
-import FileCard from "@/components/FileCard";
-import { Search } from "lucide-react";
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
-} from "@/components/ui/resizable";
-import { redirect } from "next/navigation";
-
 export default function Component() {
-  
   return <></>;
 }
